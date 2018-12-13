ecaacb0b0893af71486f527a65892ef6581b1839
kigh143 <ghkatende@gmail.com>
2018-12-13 14:56:34 +0300

    made initial made changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MainmenuComponent } from "./components/mainmenu/mainmenu.component";
+import { FeedsComponent } from "./components/feeds/feeds.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { ForgotpassComponent } from "./components/forgotpass/forgotpass.component";
+import { VerifyComponent } from "./components/verify/verify.component";
+
+const app_routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "verify", component: VerifyComponent },
+  { path: "forgotPassword", component: ForgotpassComponent },
+  { path: "home", component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainmenuComponent,
+    FeedsComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotpassComponent,
+    VerifyComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(app_routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
