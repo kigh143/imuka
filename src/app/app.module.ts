c7973853b595b04295b33c7f0b5b1835b12fcb17
kigh143 <ghkatende@gmail.com>
2018-12-13 17:28:53 +0300

    added more pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -12,13 +13,20 @@ import { LoginComponent } from "./components/login/login.component";
 import { SignupComponent } from "./components/signup/signup.component";
 import { ForgotpassComponent } from "./components/forgotpass/forgotpass.component";
 import { VerifyComponent } from "./components/verify/verify.component";
+import { PeopleComponent } from './components/people/people.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { EventsComponent } from './components/events/events.component';
+import { OpportunitiesComponent } from './components/opportunities/opportunities.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const app_routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "verify", component: VerifyComponent },
   { path: "forgotPassword", component: ForgotpassComponent },
-  { path: "home", component: HomeComponent }
+  { path: "dashboard", component: HomeComponent }
 ];
 
 @NgModule({
@@ -32,9 +40,16 @@ const app_routes: Routes = [
     LoginComponent,
     SignupComponent,
     ForgotpassComponent,
-    VerifyComponent
+    VerifyComponent,
+    PeopleComponent,
+    ChatComponent,
+    NotificationsComponent,
+    EventsComponent,
+    OpportunitiesComponent,
+    SettingsComponent,
+    ProfileComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(app_routes)],
+  imports: [BrowserModule, RouterModule.forRoot(app_routes), AngularFontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private _router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  login() {
+    this._router.navigate(["/dashboard"]);
   }
-
 }
