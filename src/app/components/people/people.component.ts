6b9161e7fea467914a2a57aa894d0775b8790488
kigh143 <ghkatende@gmail.com>
2018-12-16 19:20:44 +0300

    made changes

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
-
+  people = [1, 3,3,4,6,7,8,9,7,8,6,7,8,9,7,5,9,7];
   constructor() { }
 
   ngOnInit() {
