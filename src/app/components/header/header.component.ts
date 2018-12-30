8caea22076980b2f22c4ecda1d28889886b2ba0c
Vivian <viviangal20@gmail.com>
2018-12-30 14:45:56 +0300

    made chnages to app

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isCollapsed : boolean = false;
-  constructor() { }
+  constructor( public router: Router) { }
 
   ngOnInit() {
+  }
+
+  go_to_profile(){
+    this.router.navigate(['/profile'])
   }
 
 }
