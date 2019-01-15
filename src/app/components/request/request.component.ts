9a9fc64c844059c2e27da4bf3344e56f7715c954
Katende <ghkatende@gmail.com>
2019-01-15 22:15:05 +0300

    made changes

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
-  styleUrls: ['./request.component.css']
+  styleUrls: ['./request.component.scss']
 })
 export class RequestComponent implements OnInit {
-requests=[1, 2, 3, 4, 5];
-oneAtATime: boolean = true;
+  requests=[1, 2, 3, 4, 5];
+
   constructor() { }
 
   ngOnInit() {
