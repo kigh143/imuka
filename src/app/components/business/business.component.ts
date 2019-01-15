24097117fa06855ae7170f7012a4ab989ac81edf
Vivian Birungi <viviangal20@gmail.com>
2019-01-14 16:42:01 -0800

    forms

--- src/app/components/business/business.component.ts
+++ src/app/components/business/business.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
 @Component({
   selector: 'app-business',
   templateUrl: './business.component.html',
@@ -52,22 +58,47 @@ description:'The best product descriptions address your ideal buyer directly and
  },
  {
  	name:'documents',
- 	isclicked:false
+ 	isclicked:false,
   type:'documents',
   icon:'fa fa-file'
+ },
+  {
+  name:'Financial records',
+  isclicked:false,
+icon:'fa fa-credit-card',
+  type:'Financial records'
  },
  {
  	name:'team',
  	isclicked:false,
   type:'team',
-  icon:'fa fa-users'
- {
- 	name:'Financial records',
- 	isclicked:false,
-icon:'fa fa-credit-card',
-  type:'Financial records'
- }]
-  constructor() { }
+  icon:'fa fa-users'}
+],
+bizteam:any;
+bizproduct:any;
+bizdoc:any;
+  constructor(public formBuilder: FormBuilder, public router : Router) {
+   this.bizteam=this.formBuilder.group({
+   teamname:[ "", Validators.compose([ Validators.minlength(4),  Validators.required ])],
+   position:[ "", Validators.compose([ Validators.minlength(2),  Validators.required ])],
+   phonenumber: [ "", Validators.compose([ Validators.minlength(10),  Validators.required ])],
+   email: [ "", Validators.compose([ Validators.pattern("^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]+$"),  Validators.required ])],
+   address:[ "", Validators.compose([ Validators.minlength(4),  Validators.required ])],
+   image:[]
+   });
+
+    this.bizdoc=this.formBuilder.group({
+   type:[ "", Validators.compose([ Validators.minlength(4),  Validators.required ])],
+   docupload:[ "",Validators.required ],
+  
+   });
+   
+    this.bizproduct=this.formBuilder.group({
+   pdtname:[ "", Validators.compose([ Validators.minlength(4),  Validators.required ])],
+   descript:[ "", Validators.compose([ Validators.minlength(2),  Validators.required ])],
+   pdtimage:[]
+   });
+   }
 
   ngOnInit() {
   }
@@ -94,4 +125,28 @@ icon:'fa fa-credit-card',
   	}
 
   }
+addpdt(){
+  if (this.bizproduct.valid) {
+    this.router.navigate(['/dashboard'])
+    console.log("Form Submitted!");
+    this.bizproduct.reset();
+  }
+}
+adddoc(){
+  if (this.bizdoc.valid) {
+      this.router.navigate(['/dashboard'])
+      console.log("Form Submitted!");
+      this.bizdoc.reset();
+  }
+}
+addfinancial(){
+
+}
+addteam(){
+  if (this.bizteam.valid) {
+    this.router.navigate(['/dashboard'])
+    console.log("Form Submitted!");
+    this.bizteam.reset();
+  }
+}
 }
--- src/app/components/forgotpass/forgotpass.component.ts
+++ src/app/components/forgotpass/forgotpass.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
 @Component({
   selector: 'app-forgotpass',
   templateUrl: './forgotpass.component.html',
   styleUrls: ['./forgotpass.component.scss']
 })
 export class ForgotpassComponent implements OnInit {
-
-  constructor() { }
+myForm: any;
+  constructor(public formBuilder: FormBuilder, public router : Router) { 
+this.myForm = this.formBuilder.group({
+      email: [ "", Validators.compose([ Validators.pattern("^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]+$"),  Validators.required ])],
+     
+    });
+  }
 
   ngOnInit() {
   }
+
+forgotpass(){
+	if (this.myForm.valid) {
+  this.router.navigate(["/login"]);
+    
+      this.myForm.reset();
+    }
+}
 
 }
