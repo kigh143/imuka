3ae208064938203793d252a89bfc3cb6c8717113
Vivian Birungi <viviangal20@gmail.com>
2019-01-16 12:39:22 -0800

    forms

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.css']
 })
 export class RequestComponent implements OnInit {
-requests=[1, 2, 3, 4, 5];
-oneAtATime: boolean = true;
-  constructor() { }
+	requests=[1, 2, 3, 4, 5];
+	oneAtATime: boolean = true;
+	makerequest:any;
 
-  ngOnInit() {
+	constructor(public formBuilder: FormBuilder, public router : Router) {
+		this.makerequest=this.formBuilder.group({
+			requesttype:[ "", Validators.compose([ Validators.minLength(4),  Validators.required ])],
+			requesttitle:[ "", Validators.compose([ Validators.minLength(4),  Validators.required ])],
+			requestdetail:[ "", Validators.compose([ Validators.minLength(4),  Validators.required ])]
+		});
+	 }
+
+	ngOnInit() {
   }
+
+	sendrequest(){
+	if(this.makerequest.valid){
+		this.router.navigate(['/dashboard'])
+	    console.log("Form Submitted!");
+	    this.makerequest.reset();
+	}
+	}
 
 }
