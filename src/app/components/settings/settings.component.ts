ceb314382c111c984c14069b2023feb0f0bb3b9a
Vivian Birungi <viviangal20@gmail.com>
2019-01-17 17:24:42 -0800

    more on layout

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -8,10 +8,48 @@ import { TabsetComponent } from 'ngx-bootstrap';
 })
 export class SettingsComponent implements OnInit {
   oneAtATime: boolean = true;
+  nots:any;
+  notifications=[{
+	name:"on event posted",
+	isclicked:true
+  },
+{
+	name:"on opportunity posted",
+	isclicked:true
+  },
+  {
+	name:"investment request",
+	isclicked:true  },
+  {
+	name:"followed update",
+	isclicked:true
+  },
+  {
+	name:"connection request",
+	isclicked:true
+  }
+  ]
+			
 
-  constructor() { }
+  constructor() { 
+   this.nots=this.notifications;
+  }
 
   ngOnInit() {
+  }
+
+  turnn(i){
+  for(let j=0; j<this.notifications.length; j++){
+   if(i==j){
+    this.notifications[j].isclicked= true; 
+ 
+
+   }
+   else{
+         this.notifications[j].isclicked= false; 
+   }
+  }
+  	
   }
 
 }
