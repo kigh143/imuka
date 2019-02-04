e8d6306a116e521674d064dda0775eb0bbea16c1
Katende <ghkatende@gmail.com>
2019-02-04 11:03:37 +0300

    made chnages to profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from "../../provider/session.service";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  user : any;
+  constructor(public sessionService : SessionService ) { }
 
-  constructor() { }
+  name: string;
+  email: string;
+  phone: string;
+  address : string;
+  dob: string;
+  mstatus: string;
+  gender: string;
+  education: string;
+  relevant_exp: string;
+  relevant_skills: string;
+  expertise: string;
 
   ngOnInit() {
+    this.sessionService.getuser().subscribe( data  => {
+      this.user = data;
+    })
+  }
+
+  save(value){
+    if( value==="name"){
+
+    }else if( value==="contact"){
+      
+    }else if( value==="personnal"){
+      
+    }else if( value==="education"){
+      
+    }else if( value==="experience"){
+      
+    }
   }
 
 }
