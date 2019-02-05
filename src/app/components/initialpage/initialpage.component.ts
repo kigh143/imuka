06528e70fe913a3b0797b4ad9a7e3e57b3c1c871
Katende <ghkatende@gmail.com>
2019-02-05 17:14:10 +0300

    added two new pages

--- src/app/components/initialpage/initialpage.component.ts
+++ src/app/components/initialpage/initialpage.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-initialpage',
   templateUrl: './initialpage.component.html',
-  styleUrls: ['./initialpage.component.css']
+  styleUrls: ['./initialpage.component.scss']
 })
 export class InitialpageComponent implements OnInit {
 
--- src/app/components/orglogin/orglogin.component.ts
+++ src/app/components/orglogin/orglogin.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-orglogin',
   templateUrl: './orglogin.component.html',
-  styleUrls: ['./orglogin.component.css']
+  styleUrls: ['./orglogin.component.scss']
 })
 export class OrgloginComponent implements OnInit {
+  log_password: string;
+  log_email: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    let org = {
+      email:this.log_email,
+      log_password:this.log_password
+    }
   }
 
 }
