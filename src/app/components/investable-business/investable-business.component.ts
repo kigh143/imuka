67c5dab25524fd3056d0eaf2cde47ce82b752192
viviangal <viviangal20@gmail.com>
2019-02-19 00:34:14 -0800

    single business on investor side

--- src/app/components/investable-business/investable-business.component.ts
+++ src/app/components/investable-business/investable-business.component.ts
@@ -42,9 +42,11 @@ export class InvestableBusinessComponent implements OnInit {
   linechart: Chart;
   mline: Chart;
   piechart: Chart;
+  piechart2:any;
   sector_info:any;
   financials:any;
   parameter:any;
+  competitive:any;
 
   updates_form: any;
   months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
@@ -69,6 +71,75 @@ export class InvestableBusinessComponent implements OnInit {
         name: 'Success fee',
         complete: true
         }
+    ];
+    social_impacts = [
+      { selected: false, ans: "Security" },
+      { selected: false, ans: "Self esteem" },
+      { selected: false, ans: "Educational improvement" },
+      { selected: false, ans: "It promotes girl education" },
+      {
+        selected: false,
+        ans: "It promotes independence of the people especially women"
+      },
+      { selected: false, ans: "Health improvement" },
+      { selected: false, ans: "Food security" },
+      { selected: false, ans: "Reduce unemployment through job creation" },
+      {
+        selected: false,
+        ans: "Female engagement in employment/ gender equality"
+      },
+      {
+        selected: false,
+        ans: "It improves the living standards and well being of the people"
+      },
+      { selected: false, ans: "Reduces involvement of people in crimes" },
+      { selected: false, ans: "Reduces idleness" },
+      { selected: false, ans: "Improves the social infrastructure of the area" },
+      { selected: false, ans: "It extends social services to the people" },
+      {
+        selected: false,
+        ans: "It promotes justice and fairness with in the people"
+      },
+      { selected: false, ans: "It improves the confidence of the people" },
+      {
+        selected: false,
+        ans: "It improves the nutritional balance of the food "
+      },
+      {
+        selected: false,
+        ans:
+          "It promotes technological improvement (such as use of computer and merchinery) and innovation"
+      }
+    ];
+  
+    economic_impact = [
+      { selected: false, ans: "It provides jobs to the people" },
+      { selected: false, ans: "It increases the incomes of the people" },
+      {
+        selected: false,
+        ans: "Improvement in the standards of living of the people"
+      },
+      {
+        selected: false,
+        ans: "It provides cheaper and affordable products/ services to the people"
+      }
+    ];
+  
+    env_impact = [
+      {
+        selected: false,
+        ans: " It encourages afforestation and preservation of plantations"
+      },
+      {
+        selected: false,
+        ans:
+          " it encourages preservation of swamps and reduce environment degredation"
+      },
+      {
+        selected: false,
+        ans: " it improves waste management within the area/ reduces pollution"
+      },
+      { selected: false, ans: " it promotes recycling of the waste/ bi products" }
     ];
 
   constructor(
@@ -187,7 +258,7 @@ plotOptions: {
     }
 },
 series: [{
-    name: 'Market sixe',
+    name: 'Market size',
     
     data: [{
         name: 'current market',
@@ -203,8 +274,94 @@ series: [{
     }]
 }]
 });  
+let piechart2 = new Chart({
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+},
+title: {
+    text: 'Investment need representation'
+},
+tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+},
+plotOptions: {
+    pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: false
+        },
+        showInLegend: true
+    }
+},
+series: [{
+    name: 'Market size',
+    
+    data: [{
+        name: 'marketing',
+        y: 61.41,
+        sliced: true,
+        selected: true
+    }, {
+        name: 'Stock',
+        y: 50.84
+    }, {
+        name: 'operations',
+        y: 10.85
+    },
+    {
+      name: 'Machinery',
+      y: 40.85
+  }]
+}]
+}); 
+
+let competitive = new Chart({
+  title: {
+    text: 'Feclabs Vs Competitors'
+},
+
+
+
+yAxis: {
+    title: {
+        text: 'competitve advantage'
+    }
+},
+legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle'
+},
+
+
+
+series: [{
+    name: 'Feclabs',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+}, {
+    name: 'Competitor1',
+    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+}, {
+    name: 'Competitor1',
+    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+}, {
+    name: 'Competitor1',
+    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+}, {
+    name: 'Others',
+    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+}],
+
+
+});
     this.chart = chart;
     this.piechart = piechart;
+    this.piechart2 = piechart2;
+    this.competitive = competitive;
     this.mline = mline;
     this.linechart= linechart;
     chart.ref$.subscribe(console.log);
