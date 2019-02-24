c72ff752b032d95d27ed48e260a35f46cd830653
Katende <ghkatende@gmail.com>
2019-02-24 22:42:39 +0300

    made chnges to code

--- src/app/components/pitchbook/pitchbook.component.ts
+++ src/app/components/pitchbook/pitchbook.component.ts
@@ -18,6 +18,78 @@ export class PitchbookComponent {
   business_id: any;
   pitch: any;
   edit = false;
+  products: any;
+
+
+  social_impacts = [
+    { selected: false, ans: 'Security' },
+    { selected: false, ans: 'Self esteem' },
+    { selected: false, ans: 'Educational improvement' },
+    { selected: false, ans: 'It promotes girl education' },
+    {
+      selected: false,
+      ans: 'It promotes independence of the people especially women'
+    },
+    { selected: false, ans: 'Health improvement' },
+    { selected: false, ans: 'Food security' },
+    { selected: false, ans: 'Reduce unemployment through job creation' },
+    {
+      selected: false,
+      ans: 'Female engagement in employment/ gender equality'
+    },
+    {
+      selected: false,
+      ans: 'It improves the living standards and well being of the people'
+    },
+    { selected: false, ans: 'Reduces involvement of people in crimes' },
+    { selected: false, ans: 'Reduces idleness' },
+    { selected: false, ans: 'Improves the social infrastructure of the area' },
+    { selected: false, ans: 'It extends social services to the people' },
+    {
+      selected: false,
+      ans: 'It promotes justice and fairness with in the people'
+    },
+    { selected: false, ans: 'It improves the confidence of the people' },
+    {
+      selected: false,
+      ans: 'It improves the nutritional balance of the food '
+    },
+    {
+      selected: false,
+      ans:
+        'It promotes technological improvement (such as use of computer and merchinery) and innovation'
+    }
+  ];
+
+  economic_impact = [
+    { selected: false, ans: 'It provides jobs to the people' },
+    { selected: false, ans: 'It increases the incomes of the people' },
+    {
+      selected: false,
+      ans: 'Improvement in the standards of living of the people'
+    },
+    {
+      selected: false,
+      ans: 'It provides cheaper and affordable products/ services to the people'
+    }
+  ];
+
+  env_impact = [
+    {
+      selected: false,
+      ans: ' It encourages afforestation and preservation of plantations'
+    },
+    {
+      selected: false,
+      ans:
+        ' it encourages preservation of swamps and reduce environment degredation'
+    },
+    {
+      selected: false,
+      ans: ' it improves waste management within the area/ reduces pollution'
+    },
+    { selected: false, ans: ' it promotes recycling of the waste/ bi products' }
+  ];
 
   constructor(
     private modalService: BsModalService,
@@ -37,6 +109,7 @@ export class PitchbookComponent {
     this.businessService.fetch_abusiness( business_id ).subscribe( data => {
         this.business = data.business_info;
         this.pitch  = data.pitch;
+        this.products  = data.products;
         this.edit = false;
     });
   }
