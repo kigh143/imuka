58d11ba6a90c73bba546124ae96f8cce5de4eb32
Katende Hakim <ghkatende@gmail.com>
2019-03-25 03:44:03 +0300

    made chnages to application

--- src/app/components/investmentopp/investmentopp.component.ts
+++ src/app/components/investmentopp/investmentopp.component.ts
@@ -32,10 +32,6 @@ export class InvestmentoppComponent implements OnInit {
       offer:["", Validators.required],
       comment:[""]
     });
-    
-   
-
-     
    }
 
   ngOnInit() {
@@ -43,58 +39,49 @@ export class InvestmentoppComponent implements OnInit {
     this.currentuser=data;
     this.get_investmentable_business(this.currentuser.user_id);
   }
+
   get_investmentable_business(user_id){
     this.business_service.get_investment_opp(user_id).subscribe(data=>{
-      console.log(data);
-      this.investmentopp=data;
-      this.business_info = this.investmentopp.business_info
-      this.business_id = this.investmentopp.business_id;
-      this.sector_info = JSON.parse(this.business_info.sectors)
-      console.log( this.sector_info);
+        this.investmentopp=data;
+        this.business_info = this.investmentopp.business_info
+        this.business_id = this.investmentopp.business_id;
+        this.sector_info = JSON.parse(this.business_info.sectors);
+    });
   }
-    );
-}
+
 openModal(template: TemplateRef<any>, mdbodal:string, business) {
   this.modalRef = this.modalService.show(template);
   this.active_business = business;
-
   if(mdbodal == 'follow'){
     this.modalbody = 'follow' ;
-    
   }else if(mdbodal == 'view'){
     this.modalbody = 'view' ;
   }else{
     this.modalbody = 'invest' ;
-
   }
 }
+
 follow_business(){
   let investor= {};
   investor['user_id']=this.currentuser.user_id;
   investor['business_id']= this.active_business.business_id
-  console.log(investor);
   this.business_service.follow_business(investor).subscribe(data=>{
     if(data.flag){
       this.get_investmentable_business(this.currentuser.user_id);
       this.modalRef.hide();
-      //increase the count for follow
-      //notification "you have followed "
     }
-  })
-  
+  });
 }
+
+
 makearequest(){
   this.spinnerService.show();
   let invest_request= this.request.value;
   invest_request['user_id']= this.currentuser.user_id;
-  
-  console.log(invest_request);
   this.business_service.sendinvestrequest(invest_request).subscribe(data=>{
     this.spinnerService.hide();
     if(data.flag){
       this.request.reset();
-     
-      //add more code
     }
   });
 
