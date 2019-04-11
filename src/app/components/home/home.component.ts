25f5ee6be25fa6057b4cbaf5c25a266b4db812fb
viviangal <viviangal20@gmail.com>
2019-04-11 14:19:48 +0300

    added counters

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from '../../provider/session.service';
+import { BizService } from 'src/app/provider/biz.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,10 @@ import { SessionService } from '../../provider/session.service';
 export class HomeComponent implements OnInit {
   user: any;
   header: string;
-  constructor( public router: Router,  public sessionService: SessionService) { }
+  myinvestment_count;
+  request_count;
+  count;
+  constructor( public router: Router,  public sessionService: SessionService, public businessService:BizService) { }
 
   ngOnInit() {
     const data = this.sessionService.getuser();
@@ -20,10 +24,21 @@ export class HomeComponent implements OnInit {
     } else {
       this.header = 'Welcome to imukaAccess';
     }
+    this.getcount();
   }
 
   navigate_to(page) {
     this.router.navigate([page]);
   }
+  getcount(){
+    this.businessService.getcount(this.user.user_id).subscribe(data => {
+       this.count= data;
+       this.myinvestment_count = this.count.my_investment;
+       this.request_count = this.count.investment_requests;
 
+       console.log(this.myinvestment_count + "HELLO");
+
+    });
+
+}
 }
--- src/app/components/mainmenu/mainmenu.component.ts
+++ src/app/components/mainmenu/mainmenu.component.ts
@@ -15,6 +15,8 @@ export class MainmenuComponent implements OnInit {
   event_counter :any;
   opps_count : any;
   business_count : any;
+  myinvestment_count:any;
+  investment_count:any;
 
 
   constructor(public router : Router, public sessionService :SessionService, public businessService:BizService ) { }
@@ -26,8 +28,12 @@ export class MainmenuComponent implements OnInit {
   getcount(){
     this.businessService.getcount(this.user.user_id).subscribe(data => {
        this.count = data;
+       console.log(this.count);
        this.event_counter = data.events;
        this.opps_count = data.opps;
+       this.myinvestment_count = data.my_investment;
+       this.investment_count = data.investments
+
        this.business_count = data.businesses;
        console.log(this.count);
 
