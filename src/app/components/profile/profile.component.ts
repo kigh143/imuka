889f2dc4fc186ba0d74fef9f69157b959c750bb4
ivan-mp <ghkatende@gmail.com>
2019-07-24 21:51:25 +0300

    gdhf bhdjfhdf

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef} from '@angular/core';
 import { SessionService } from '../../provider/session.service';
 import { AuthService } from '../../provider/auth.service';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
